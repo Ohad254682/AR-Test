916c281bf55b3d3dc372804fdcbaca23a61af6ec
unknown <ohadavidar@gmail.com>
2020-04-06 12:39:47 +0300

    f

---
+++ index.js
@@ -0,0 +1,29 @@
+//To avoid playing from start if the marker flickers, not sure if needed, but it might be a failsafe when you have mutiple markers and want to make sure the first finishes before launching the second
+var playing = false;
+
+//HTML5 audio, will need user touch input to start on mobile
+var intro = new Audio("Trex.mp3");
+
+//Detect end of audio
+intro.addEventListener("ended", function() {
+	intro.currentTime = 0;
+	playing = false;
+});
+
+AFRAME.registerComponent('markerhandler', {
+	init: function() {
+		// Set up the tick throttling. Will check if marker is active every 500ms
+		this.tick = AFRAME.utils.throttleTick(this.tick, 500, this);
+	},
+	tick: function(t, dt) {
+		
+		if (document.querySelector("#marker").object3D.visible == true && playing == false) {
+			// MARKER IS PRESENT
+			intro.play();
+			playing = true;
+		} else {
+			// MARKER IS HIDDEN, do nothing (up to you)
+		}
+
+	}
+});
